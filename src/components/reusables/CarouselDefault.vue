8fcefc69d96c4ec33edb0e4482ad389bb88629df
Guilherme Granzeol <guilhermegranzeol@MacBook-Pro-de-Guilherme.local>
2019-10-24 23:51:18 -0300

    altercao

--- src/components/reusables/CarouselDefault.vue
+++ src/components/reusables/CarouselDefault.vue
@@ -1,44 +1,51 @@
 <template> 
-  <div> 
-    <v-carousel height="60vh">
+  <div>
       <v-carousel-item
-        src="https://cdn.pixabay.com/photo/2015/10/12/15/18/clothing-store-984396__340.jpg"
-  reverse-transition="fade-transition"
+        :src="src"
+        reverse-transition="fade-transition"
         transition="fade-transition"
       >
         <v-row justify="end" align="center">
           <v-col md="5" col="5" class="text" align="center">
-            <div >
-              <h3 :class="titleClass">TESTE</h3>
-              <p :class="textClass">Lorem ipsum dolor sincidunt vel soluta distinctio quia veritatis non dolores nemo. Necessitatibus?</p>
-              <button :class="buttonClass">Go</button>
+            <div  id="carousel-text">
+              <h2 >{{ title }}</h2>
+              <p >{{ text }}</p>
+              <button id="corousel-button">{{ buttonText}}</button>
             </div>
           </v-col>
         </v-row>
         <v-sheet></v-sheet>
       </v-carousel-item>
-    </v-carousel>
   </div>
 </template>
 
 
 <script>
 export default {
-  data() { //Finalizar Carousel
+  data() { //Corrigir alinhamento Carousel
+          //Colocar click botao
     return {};
+  },
+  props: {
+    src: {String},
+    title: {String},
+    text: {String },
+    buttonText:{String}
   }
 };
 </script>
 
 <style scoped>
 .text {
-  color: white;
-  padding: 10vh 12vw 0 0;
+  color: black;
+  padding: 12vw 5vw 0 5vw;
+  align-items: center;
+  background: rgba(216, 203, 203, 0.76);
+  height: 70vh;
 }
 
 .text button{
-    background: red;
-    padding: 5px;
-    border-radius: 8px
+    padding: 10px;
+    border-radius: 15px;  
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,33 @@
 <template>
-    <admin>
-        <carousel-default></carousel-default>
-    </admin>
+  <admin>
+    <v-carousel height="70vh">
+      <carousel-default
+        src="https://cdn.pixabay.com/photo/2016/01/27/04/32/books-1163695_960_720.jpg"
+        title="Loren Ip"
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, dolor delectus reiciendis sequi ducimus quo rerum, laboriosam perferendis, facilis quia blanditiis! Id eius, cumque totam vel eveniet debitis libero sint."
+        buttonText="Hello World"
+      />
+      <carousel-default
+        src="https://cdn.pixabay.com/photo/2017/10/13/05/26/silk-tie-2846862_960_720.jpg"
+        title="Other"
+        text="Lorem ipsum or delectus reiciendis sequi ducimus quo rerum, laboriosam perferendis, facilis quia blanditiis! Id eius, cumque totam vel eveniet debitis libero sint."
+        buttonText="Other"
+      />
+    </v-carousel>
+  </admin>
 </template>
 
 <script>
-import Admin from '@/templates/Admin'
-import CarouselDefault from '@/components/reusables/CarouselDefault'
+import Admin from "@/templates/Admin";
+import CarouselDefault from "@/components/reusables/CarouselDefault";
 
 export default {
-    name: 'Home',
-    components:{
-       Admin,
-       CarouselDefault
-    }
+  name: "Home",
+  components: {
+    Admin,
+    CarouselDefault
+  }
 };
 </script>
 <style scouped>
-
 </style>
